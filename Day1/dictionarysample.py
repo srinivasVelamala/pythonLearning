0f5a68f3725c08b562fb24625198b1a3516e2d28
srinivasv18 <srinivasv18@gmail.com>
2024-06-18 10:46:02 +0530

    dictionary samples

--- Day1/dictionarysample.py
+++ Day1/dictionarysample.py
@@ -10,5 +10,14 @@ print(student["name"])  # Output: Alice
 print(student["age"])   # Output: 20
 print(student["major"])  # Output: Computer Science
 
-del student["name"]
+student = {
+    "name": "Alice",
+    "age": 20,
+    "major": "Computer Science"
+}
+for key,value in student.items():
+    if value == 'Alice':
+        del student['name']
+        break
+print('After deletion')
 print(student)
