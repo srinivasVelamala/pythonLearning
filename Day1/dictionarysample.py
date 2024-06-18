2b18276e6ec1bcde9b396d582d56986beef9b1c0
srinivasv18 <srinivasv18@gmail.com>
2024-06-18 10:43:14 +0530

    dictionary samples

---
+++ Day1/dictionarysample.py
@@ -0,0 +1,14 @@
+# Creating a dictionary
+student = {
+    "name": "Alice",
+    "age": 20,
+    "major": "Computer Science"
+}
+
+# Accessing dictionary values
+print(student["name"])  # Output: Alice
+print(student["age"])   # Output: 20
+print(student["major"])  # Output: Computer Science
+
+del student["name"]
+print(student)
