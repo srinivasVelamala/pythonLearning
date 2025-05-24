d11af855cefbc694d21b1734f929ca1f5cc3be36
srinivasVelamala <srinivasv18@gmail.com>
2025-05-24 14:28:19 +0530

    numpy and pandas notes

---
+++ numpy/numpsample1.py
@@ -0,0 +1,14 @@
+import numpy as np
+n1=np.array([10,20,30,40])
+print(n1)
+#multidimensional array
+n2=np.array([[10,20,30],[40,50,60]])    
+print(n2)
+#creating array with zeros
+n3=np.zeros((2,3))  #2 rows and 3 columns   
+print(n3)
+#creating array with ones
+n4=np.full((2,3),9)  #2 rows and 3 columns
+print(n4)
+#array transpose
+n
